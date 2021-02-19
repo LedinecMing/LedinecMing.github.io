f9b197882211ef941aac5fcf22d6255883df9466
LedinecMing <63245550+LedinecMing@users.noreply.github.com>
2021-02-19 09:28:15 +0400

    Add files via upload

--- Pages/lol2.js
+++ Pages/lol2.js
@@ -776,7 +776,7 @@ let builds=[0, new Build(7, 10, 1, 3, 0, 0, [[1, 19]], false, 0, [0]), new Build
       console.log(thing)
       for (var i = 0; i < thing[2]; i++) {
         for (var j = 0; j < thing[2]; j++) {
-          if(i**2+j**2<thing[2]**2)
+          if((i-thing/2)**2+(j-thing/2)**2<thing[2]**2)
           {
             world.map[Math.round(Math.abs(pos[0]+i-thing[2]/2)%size)][Math.round(Math.abs(pos[1]+j-thing[2]/2)%size)]=1;
           }
